911c41f6adc0e06455612406fc1b2a401170fbac
m.azzam <m.azzam.azis@gmail.com>
2024-12-01 18:12:45 +0700

    refactor: public folder and templates

--- public/background.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === "sendSelection") {
-    console.log(`[Action: ${message.action}] Text:`, message.text);
-    sendResponse({ message: "Selection received" });
-  }
-});
