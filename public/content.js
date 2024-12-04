ff27fdc8dbdda4c3b0a7a2e24642351f58415b5c
m.azzam <m.azzam.azis@gmail.com>
2024-12-04 13:41:39 +0700

    pref

--- public/content.js
+++ public/content.js
@@ -105,13 +105,14 @@ const addTextSelectionListener = () => {
           try {
             const results = await detector.detect(selectedText);
             const detectedLanguage = results[0].detectedLanguage;
+            preferredLang = await getPreferredLanguage();
             outputFooter.textContent = `Language detected: ${detectedLanguage}`;
+            outputFooterPreferred.textContent = `Language preferred: ${preferredLang}`;
 
             // Start streaming the summary
             const stream = summarizer.summarizeStreaming(selectedText);
 
             output.textContent = "Generating response...";
-            preferredLang = await getPreferredLanguage();
             console.log("detect: " + detectedLanguage);
             console.log("preferred: " + preferredLang);
 
@@ -201,16 +202,26 @@ const closeButton = document.createElement("button");
 closeButton.id = "clariff-close-btn";
 
 const closeIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-closeIcon.setAttribute("viewBox", "0 0 384 512");
-closeIcon.innerHTML =
-  '<path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>';
+closeIcon.setAttribute("viewBox", "0 0 24 24");
+closeIcon.innerHTML = `<path
+    d="M6 15L12 9L18 15"
+    stroke="#000000"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />`;
+
 closeButton.appendChild(closeIcon);
 
 closeButton.addEventListener("click", () => {
-  cardContainer.hidden = !cardContainer.hidden;
+  const root = document.getElementById("clariff-root");
+  const isHidden = root.hidden;
+  root.hidden = !isHidden;
+  // Toggle chevron rotation on button click
+  closeButton.classList.toggle("open");
 });
-cardHeader.appendChild(closeButton);
 
+cardHeader.appendChild(closeButton);
 cardContainer.appendChild(cardHeader);
 
 // Text area where summarizations are displayed
@@ -229,6 +240,11 @@ const outputFooter = document.createElement("p");
 outputFooter.id = "clariff-language";
 outputFooter.textContent = "Language detected: ";
 outputContainer.appendChild(outputFooter);
+
+const outputFooterPreferred = document.createElement("p");
+outputFooterPreferred.id = "clariff-language-select";
+outputFooterPreferred.textContent = "Language preferred: ";
+outputContainer.appendChild(outputFooterPreferred);
 
 const buttonGroup = document.createElement("div");
 buttonGroup.id = "clariff-btn-group";
